6ed913b16c2c7701759d313cb6e0113298f07a75
“Mynorka <mynorkadaza@gmail.com>
2024-07-25 15:38:18 +0200

    auth routes

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use("/api",skillRequestRoutes);
 const reviewRoutes = require("./routes/review.routes");
 app.use("/api", reviewRoutes);
 
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes)
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
---
+++ routes/auth.routes.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model")
+
+const router = express.Router();
+const {isAuthenticated} = require("./../middleware/jwt.middleware")
+const saltRounds = 10;
+
+router.post("/signup", (req, res) =>{
+    const {email, password, fullName} = req.body;
+
+    if(email === " " || password === " " || fullName === " "){
+        res.status(400).json({message: "Provide email, password, and name"});
+        return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if(!emailRegex.test(email)){
+        res.status(400).json({message:"Provide a valid email address."})
+        return
+    }
+
+    User.findOne({email})
+    .then((foundUser)=>{
+        if(foundUser){
+            res.status(400).json({message:"User already exists"});
+            return;
+        }
+
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPasword = bcrypt.hashSync(password, salt)
+
+        return User.create({email, password: hashedPasword, fullName})
+    })
+    .then((createdUser) =>{
+        const {email, fullName, _id} = createdUser
+
+        const user = {email, fullName, _id};
+
+        res.status(201).json({user:user})
+    })
+    .catch(err =>{
+        res.status(500).json({message:"internal server error"})
+    })
+})
+
+router.post("/login", (req, res)=>{
+    const {email, password} = req.body;
+
+    if (email === " " || password === " "){
+        res.status(400).json({message:"Provide email and password."});
+        return;
+    }
+
+    User.findOne({email})
+    .then((foundUser)=>{
+
+        if (!foundUser){
+            res.status(401).json({message:"User not found"})
+            return;
+        }
+
+        const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+
+        if(passwordCorrect){
+            const {_id, email, fullName} = foundUser;
+
+            const payload = {_id, email, fullName};
+
+            const authToken = jwt.sign(
+                payload, process.env.TOKEN_SECRET, {algorithm: "HS256", expiresIn:"12h"}
+            );
+
+            res.status(200).json({authToken:authToken})
+        }
+        else{
+            res.status(401).json({message: "unable to authenticate the user"})
+        }
+    })
+    .catch(err => res.status(500).json({message: "internal server error"}))
+
+})
+
+router.get("/verify", isAuthenticated, (req,res) =>{
+    console.log("req.payload", req.payload);
+    res.status(200).json(req.payload)
+});
+
+module.exports = router
